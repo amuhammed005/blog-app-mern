6caa9b21b2d4ebf2a7b97ea37acf5f8c638dc947
amuhammed005 <amuhammed005@st.ug.edu.gh>
2025-01-14 16:41:47 +0000

    created pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,24 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./assets/pages/Home";
+import SignIn from "./assets/pages/SignIn";
+import About from "./assets/pages/About";
+import Projects from "./assets/pages/Projects";
+import Dashboard from "./assets/pages/Dashboard";
+import SignUp from "./assets/pages/SignUp";
+
 const App = () => {
-  return <div className="text-3xl text-blue-900">App</div>;
+  return (
+    <BrowserRouter className="text-3xl text-blue-900">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
