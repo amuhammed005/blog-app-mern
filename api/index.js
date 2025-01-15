f5b5ad941e7a4efac0fa2e527036a059b456fae8
amuhammed005 <amuhammed005@st.ug.edu.gh>
2025-01-15 02:01:15 +0000

    add a middleware and a fucntion to handle errors

--- api/index.js
+++ api/index.js
@@ -15,6 +15,16 @@ app.use(express.json());
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal server error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
+
 app.listen(port, () => {
   console.log(`Server started on http://localhost:${port}`);
 });
