e54b9365552605eb0840612c4c013542a9d397f5
amuhammed005 <amuhammed005@st.ug.edu.gh>
2025-01-15 00:23:58 +0000

    create a test api route

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,3 @@
+export const getUser = async (req, res) => {
+  res.send({ message: "Test controller is ok" });
+};
--- api/index.js
+++ api/index.js
@@ -1,12 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDb from "./config/db.js";
+import userRoutes from "./routes/user.route.js";
 
 dotenv.config();
 connectDb();
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use("/api/user", userRoutes);
 
 app.listen(port, () => {
   console.log(`Server started on http://localhost:${port}`);
