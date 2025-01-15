74d158881a5f7085d9a99b041df3157b3e32d830
amuhammed005 <amuhammed005@st.ug.edu.gh>
2025-01-15 01:40:47 +0000

    create signup api route

--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDb from "./config/db.js";
-import userRoutes from "./routes/user.route.js";
+import userRoutes from "./routes/user.routes.js";
+import authRoutes from "./routes/auth.routes.js";
 
 dotenv.config();
 connectDb();
@@ -9,7 +10,10 @@ connectDb();
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.json());
+
 app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
 
 app.listen(port, () => {
   console.log(`Server started on http://localhost:${port}`);
