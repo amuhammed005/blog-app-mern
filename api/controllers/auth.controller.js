913ad8e7ffe7bc43b51ef5f531b937c0a5c0784a
amuhammed005 <amuhammed005@st.ug.edu.gh>
2025-01-16 23:53:14 +0000

    add google auth functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -54,3 +54,50 @@ export const signin = async (req, res, next) => {
     next(error);
   }
 };
+
+export const google = async (req, res, next) => {
+  const { email, name, googlePhotoURL } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (user) {
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES_IN,
+      });
+      const { password, ...rest } = user._doc;
+      res
+        .status(200)
+        .cookie("access_token", token, { httpOnly: true })
+        .json(rest);
+    } else {
+      const generatedPassword =
+        Math.random().toString(36).slice(-8) +
+        Math.random().toString(36).slice(-8);
+      const hashedPassword = bcryptjs.hashSync(generatedPassword, 10);
+      const username =
+        name.toLowerCase().split(" ").join("") +
+        Math.floor(Math.random() * 10000);
+
+      const newUser = new User({
+        username,
+        email,
+        password: hashedPassword,
+        profilePhoto: googlePhotoURL,
+      });
+      await newUser.save();
+      const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES_IN,
+      });
+      const { password, ...rest } = newUser._doc;
+      res
+        .status(200)
+        .cookie("access_token", token, {
+          httpOnly: true,
+        })
+        .json(rest);
+    }
+  } catch (error) {
+    console.error("Error in Google Auth:", error);
+    next(error);
+  }
+};
